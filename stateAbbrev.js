07aeba7d4ab9f620654d21ecf886d1cde4fdc458
cmacdo1 <chrismacdonaldphotography@yahoo.com>
2023-11-17 21:54:12 -0700

    Changed 'const' to 'var' and added error handling

--- stateAbbrev.js
+++ stateAbbrev.js
@@ -1,77 +1,74 @@
 function abbreviateState(address) {
-    const stateAbbreviations = {
-        'Alabama': 'AL',
-        'Alaska': 'AK',
-        'Arizona': 'AZ',
-        'Arkansas': 'AR',
-        'California': 'CA',
-        'Colorado': 'CO',
-        'Connecticut': 'CT',
-        'Delaware': 'DE',
-        'Florida': 'FL',
-        'Georgia': 'GA',
-        'Hawaii': 'HI',
-        'Idaho': 'ID',
-        'Illinois': 'IL',
-        'Indiana': 'IN',
-        'Iowa': 'IA',
-        'Kansas': 'KS',
-        'Kentucky': 'KY',
-        'Louisiana': 'LA',
-        'Maine': 'ME',
-        'Maryland': 'MD',
-        'Massachusetts': 'MA',
-        'Michigan': 'MI',
-        'Minnesota': 'MN',
-        'Mississippi': 'MS',
-        'Missouri': 'MO',
-        'Montana': 'MT',
-        'Nebraska': 'NE',
-        'Nevada': 'NV',
-        'New Hampshire': 'NH',
-        'New Jersey': 'NJ',
-        'New Mexico': 'NM',
-        'New York': 'NY',
-        'North Carolina': 'NC',
-        'North Dakota': 'ND',
-        'Ohio': 'OH',
-        'Oklahoma': 'OK',
-        'Oregon': 'OR',
-        'Pennsylvania': 'PA',
-        'Rhode Island': 'RI',
-        'South Carolina': 'SC',
-        'South Dakota': 'SD',
-        'Tennessee': 'TN',
-        'Texas': 'TX',
-        'Utah': 'UT',
-        'Vermont': 'VT',
-        'Virginia': 'VA',
-        'Washington': 'WA',
-        'West Virginia': 'WV',
-        'Wisconsin': 'WI',
-        'Wyoming': 'WY',
-        'District of Columbia': 'DC',
-      };      
+    var stateAbbreviations = {
+        'alabama': 'AL',
+        'alaska': 'AK',
+        'arizona': 'AZ',
+        'arkansas': 'AR',
+        'california': 'CA',
+        'colorado': 'CO',
+        'connecticut': 'CT',
+        'delaware': 'DE',
+        'florida': 'FL',
+        'georgia': 'GA',
+        'hawaii': 'HI',
+        'idaho': 'ID',
+        'illinois': 'IL',
+        'indiana': 'IN',
+        'iowa': 'IA',
+        'kansas': 'KS',
+        'kentucky': 'KY',
+        'louisiana': 'LA',
+        'maine': 'ME',
+        'maryland': 'MD',
+        'massachusetts': 'MA',
+        'michigan': 'MI',
+        'minnesota': 'MN',
+        'mississippi': 'MS',
+        'missouri': 'MO',
+        'montana': 'MT',
+        'nebraska': 'NE',
+        'nevada': 'NV',
+        'new hampshire': 'NH',
+        'new jersey': 'NJ',
+        'new mexico': 'NM',
+        'new york': 'NY',
+        'north carolina': 'NC',
+        'north dakota': 'ND',
+        'ohio': 'OH',
+        'oklahoma': 'OK',
+        'oregon': 'OR',
+        'pennsylvania': 'PA',
+        'rhode island': 'RI',
+        'south carolina': 'SC',
+        'south dakota': 'SD',
+        'tennessee': 'TN',
+        'texas': 'TX',
+        'utah': 'UT',
+        'vermont': 'VT',
+        'virginia': 'VA',
+        'washington': 'WA',
+        'west virginia': 'WV',
+        'wisconsin': 'WI',
+        'wyoming': 'WY',
+        'district of columbia': 'DC',
+    };
+    
+    var lowercaseAddress = address.toLowerCase();
   
-    // Iterate over state abbreviations
-    for (const stateName in stateAbbreviations) {
+    for (var stateName in stateAbbreviations) {
       if (stateAbbreviations.hasOwnProperty(stateName)) {
-        const abbreviation = stateAbbreviations[stateName];
+        var abbreviation = stateAbbreviations[stateName];
         
-        // Check if the state name is present in the address
-        if (address.includes(stateName)) {
-          // Replace state name with abbreviation
-          return address.replace(stateName, abbreviation);
+        if (lowercaseAddress.includes(stateName)) {
+          return address.replace(new RegExp(stateName, 'i'), abbreviation);
         }
       }
     }
-  
-    // If no state name is found or it doesn't have an abbreviation, return the original address
     return address;
   }
   
   // Example usage
-  const inputAddress = "1234 South Price Road. Tempe, District of Columbia 85281";
+  const inputAddress = "20 West 34th Street, albany, new york 10001";
   const outputAddress = abbreviateState(inputAddress);
   console.log(outputAddress);
   
